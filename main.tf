c186e70110ee43e7d3be88da8fd12f5a7df5b099
ankitchaure98 <ankitchaure98@gmail.com>
2025-09-25 13:43:06 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "zohort_bucket" {
+  bucket = "zohort-devops-aniket"
+
+  versioning {
+    enabled = true
+  }
+
+  acl = "private"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.zohort_bucket.id
+  description = "The name of the S3 bucket"
+}
